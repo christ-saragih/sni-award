766350d4df397b391036e19c42a62d6d1e9d4822
lafathalfath <iqnaraidan12@gmail.com>
2024-03-15 09:29:39 +0700

    PULL from master

--- database/migrations/2024_02_21_075913_create_peserta_table.php
+++ database/migrations/2024_02_21_075913_create_peserta_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->integerIncrements('id');
             $table->string('nama');
             $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('verify_key');
             $table->enum('status', ['aktif', 'tidak aktif']);
