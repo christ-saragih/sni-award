beac088e480192eecaba217c13e6487e6bbd95cd
Jonathan <nath.joe34@gmail.com>
2024-03-21 17:41:53 +0700

    merge fix bug acara dan assessment

--- app/Http/Controllers/Peserta/RegistrasiAssessmentController.php
+++ app/Http/Controllers/Peserta/RegistrasiAssessmentController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Peserta;
 
 use App\Models\RegistrasiAssessment;
 use App\Http\Controllers\Controller;
+use App\Models\AssessmentJawaban;
 use App\Models\AssessmentKategori;
+use App\Models\AssessmentPertanyaan;
 use Illuminate\Http\Request;
 
 class RegistrasiAssessmentController extends Controller
@@ -16,28 +18,19 @@ class RegistrasiAssessmentController extends Controller
     //     ]);
     // }
 
-    public function getAssessmentKategori() {
-        $assessment_kategori = AssessmentKategori::where('nama','LIKE', '%'.request('q').'%')->paginate(10);
-        return response()->json($assessment_kategori);
-    }
-    //
-    public function index()
-    {
-        $assessment_kategori = AssessmentKategori::get();
-        // dd($assessment_sub_kategori);
-        return view('peserta.pendaftaran.index',[
+    public function show($assessment_kategori_id, $assessment_pertanyaan_id, $assessment_jawaban_id) {
+        $assessment_kategori = AssessmentKategori::find($assessment_kategori_id);
+        $assessment_pertanyaan = AssessmentPertanyaan::find($assessment_pertanyaan_id);
+        $assessment_jawaban = AssessmentJawaban::find($assessment_jawaban_id);
+        if (!$assessment_kategori || !$assessment_pertanyaan || !$assessment_jawaban){
+            return response()->json(['error' => 'Data not found'], 404);
+        }
+        return response()->json([
             'assessment_kategori' => $assessment_kategori,
-            'list_assessment_kategori' => $assessment_kategori,
+            'assessment_pertanyaan' => $assessment_pertanyaan,
+            'assessment_jawaban' => $assessment_jawaban
         ]);
     }
-//     public function store(Request $request){
-//         $request->validate([
-//             'nama' => 'required|max:50',
-//         ]);
-//         AssessmentKategori::create([
-//             'nama' => $request->nama,
-//         ]);
 
-//         return redirect('/admin/assessment')->with('success','Kategori berhasil ditambahkan');
-//     }
+    
 }
