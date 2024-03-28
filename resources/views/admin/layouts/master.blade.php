33e426ee70e6d2622b9a700476c6b565453c276a
christ-saragih <bennefit.19@gmail.com>
2024-03-28 17:44:16 +0700

    FIXING: Master peserta

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -504,6 +504,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="{{ asset('assets') }}/admin/js/script.js"></script>
+    <script src="{{ asset('assets') }}/admin/js/app.js"></script>
 
 </body>
 
