4a4c0773cba979c2ce8192f460970ad28905e749
lafathalfath <iqnaraidan12@gmail.com>
2024-03-29 02:38:45 +0700

    UPDATE: peserta master layout

--- resources/views/peserta/layouts/master.blade.php
+++ resources/views/peserta/layouts/master.blade.php
@@ -77,7 +77,7 @@
         <a
           class="navbar-brand m-0"
           id="navbarBrand"
-          href="/peserta"
+          href="/"
         >
           <img
             src="{{ asset('assets') }}/images/icon/logo-sniaward.svg"
@@ -184,7 +184,7 @@
                    <i class="fa fa-bell-o cursor-pointer"></i>
                 </a>
                 <ul
-                  class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4"
+                  class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4 border-0"
                   aria-labelledby="dropdownMenuButton"
                 >
                   <li class="mb-2">
@@ -303,116 +303,50 @@
               </li>
               <li class="nav-item dropdown d-flex px-2 gap-1 align-items-center">
                 <a
-                  href="{{route('peserta.profil.index')}}"
-                  class="nav-link text-body p-0"
-                >
-                  <i class="fa fa-user-o me-sm-1"></i>
-                  <span class="d-sm-inline d-none me-sm-1 p-0"><b>Peserta</b></span>
-                </a>
-                <a
                   href="javascript:;"
                   class="nav-link text-body p-0"
                   id="dropdownMenuButton"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
+                  <i class="fa fa-user-o me-sm-1"></i>
+                  <span class="d-sm-inline d-none me-sm-1 p-0"><b>{{ auth()->guard('peserta')->user()->nama }}</b></span>
                   <i class="fa fa-caret-down"></i>
                 </a>
                 <ul
-                  class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4"
+                  class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4 border-0"
                   aria-labelledby="dropdownMenuButton"
                 >
-                  <li class="mb-2">
-                    <a
-                      class="dropdown-item border-radius-md"
-                      href="javascript:;"
-                    >
-                      <div class="d-flex justify-content-center">
-                        <i class="fa fa-user"></i>
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New message</span>
-                          from Laursdsafa
-                        </h6>
-                      </div>
-                      
-                    </a>
-                  </li>
-                  <li class="mb-2">
-                    <a
-                      class="dropdown-item border-radius-md"
-                      href="javascript:;"
-                    >
-                      <div class="d-flex justify-content-center">
-                        <i class="fa fa-user"></i>
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New message</span>
-                          from Laursdsafa
-                        </h6>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="dropdown-item border-radius-md"
-                      href="javascript:;"
-                    >
-                      <div class="d-flex py-1">
-                        <div
-                          class="avatar avatar-sm bg-gradient-secondary me-3 my-auto"
+                <div class="d-flex align-items-center justify-content-center">
+                  <div class="">
+                      <li class="mb-2 w-50">
+                        <a
+                          class="dropdown-item border-radius-md"
+                          href="/peserta/profil"
                         >
-                          <svg
-                            width="12px"
-                            height="12px"
-                            viewBox="0 0 43 36"
-                            version="1.1"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink"
-                          >
-                            <title>credit-card</title>
-                            <g
-                              stroke="none"
-                              stroke-width="1"
-                              fill="none"
-                              fill-rule="evenodd"
-                            >
-                              <g
-                                transform="translate(-2169.000000, -745.000000)"
-                                fill="#FFFFFF"
-                                fill-rule="nonzero"
-                              >
-                                <g
-                                  transform="translate(1716.000000, 291.000000)"
-                                >
-                                  <g
-                                    transform="translate(453.000000, 454.000000)"
-                                  >
-                                    <path
-                                      class="color-background"
-                                      d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"
-                                      opacity="0.593633743"
-                                    ></path>
-                                    <path
-                                      class="color-background"
-                                      d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z"
-                                    ></path>
-                                  </g>
-                                </g>
-                              </g>
-                            </g>
-                          </svg>
-                        </div>
-                        <div class="d-flex flex-column justify-content-center">
-                          <h6 class="text-sm font-weight-normal mb-1">
-                            Payment successfully completed
-                          </h6>
-                          <p class="text-xs text-secondary mb-0">
-                            <i class="fa fa-clock me-1"></i>
-                            2 days
-                          </p>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
+                            <div class="d-flex gap-4 align-items-center">
+                              <i class="fa fa-user" style="width: 12%;"></i>
+                              <h6 class="text-sm font-weight-normal mb-0">
+                                Profil
+                              </h6>
+                            </div>
+                        </a>
+                      </li>
+                      <li class="w-50">
+                        <a
+                          class="dropdown-item border-radius-md"
+                          href="/keluar"
+                        >
+                            <div class="d-flex gap-4 align-items-center">
+                              <i class="fa fa-sign-out" style="width: 12%;"></i>
+                              <h6 class="text-sm font-weight-normal mb-1">
+                                Keluar
+                              </h6>
+                            </div>
+                        </a>
+                      </li>
+                    </div>
+                  </div>
                 </ul>
               </li>
             </ul>
