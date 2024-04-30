4e75c8c351b0706725123b1f70789e6698fbee28
christ-saragih <bennefit.19@gmail.com>
2024-04-30 12:09:31 +0700

    FIXING: Footer guest

--- app/Http/Controllers/Guest/FaqController.php
+++ app/Http/Controllers/Guest/FaqController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Faq;
+use App\Models\Frontpage;
 use Illuminate\Http\Request;
 
 class FaqController extends Controller
 {
     public function index() {
+        $frontpage_data = Frontpage::get();
         $popular_faq = Faq::where('is_popular', true)->get();
         $unpopular_faq = Faq::where('is_popular', false)->get();
         return view('guest.faq.index', [
+            'frontpage_data' => $frontpage_data[0],
             'popular_faq' => $popular_faq,
             'unpopular_faq' => $unpopular_faq,
     ]);
--- app/Http/Controllers/Guest/LinimasaController.php
+++ app/Http/Controllers/Guest/LinimasaController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Frontpage;
 use Illuminate\Http\Request;
 
 class LinimasaController extends Controller
 {
     public function index() {
-        return view('guest.linimasa.index');
+        $frontpage_data = Frontpage::get();
+        return view('guest.linimasa.index', [
+            'frontpage_data' => $frontpage_data[0],
+        ]);
     }
 }
