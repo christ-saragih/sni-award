97f92afdd55611bf314b0b8e9ac388d3425add6f
hafisfaqih <hafis.police@gmail.com>
2024-05-08 19:11:22 +0700

    CRUS : Peserta Profil, Dokumen

--- resources/views/peserta/profil/wizard/dokumen.blade.php
+++ resources/views/peserta/profil/wizard/dokumen.blade.php
@@ -30,7 +30,9 @@
                   </div>
                   <div class="container col-md-8 pe-5">
                     @if ($peserta->peserta_profil->url_legalitas_hukum_organisasi)
-                    <a href="{{Storage::url($peserta->peserta_profil->url_legalitas_hukum_organisasi)}}" style="border: 1px solid #552525; color: #552525; padding-block: 0.5rem; font-size: 1.25rem;" class="form-control form-control-lg text-center "><i class="fa fa-download"></i></a>
+                    <a href="{{Storage::url($peserta->peserta_profil->url_legalitas_hukum_organisasi)}}" target="_blank"> 
+                      <i class="fa fa-download" aria-hidden="true" style="color: #552525; border: 2px solid #552525; border-radius: 8px; padding: 0.3rem"></i>
+                    </a>
                     @endif
                     <div class="input-group custom-file-button">
                       <label class="input-group-text px-4" for="inputGroupFile1">Unggah</label>
@@ -46,7 +48,9 @@
                   </div>                  
                   <div class="container col-md-8 pe-5">
                     @if  ($peserta->peserta_profil->url_sppt_sni)
-                    <a href="{{Storage::url($peserta->peserta_profil->url_sppt_sni)}}" style="border: 1px solid #552525; color: #552525; padding-block: 0.5rem; font-size: 1.25rem;" class="form-control form-control-lg text-center "><i class="fa fa-download"></i></a>
+                    <a href="{{Storage::url($peserta->peserta_profil->url_sppt_sni)}}"  target="_blank">
+                      <i class="fa fa-download" aria-hidden="true" style="color: #552525; border: 2px solid #552525; border-radius: 8px; padding: 0.3rem"></i>
+                    </a>
                     @endif
                     <div class="input-group custom-file-button">
                       <label class="input-group-text px-4" for="inputGroupFile2">Unggah</label>
@@ -62,7 +66,9 @@
                   </div>
                   <div class="container col-md-8 pe-5">
                     @if ($peserta->peserta_profil->url_sk_kemenkumham)
-                    <a href="{{Storage::url($peserta->peserta_profil->url_sk_kemenkumham)}}" style="border: 1px solid #552525; color: #552525; padding-block: 0.5rem; font-size: 1.25rem;" class="form-control form-control-lg text-center "><i class="fa fa-download"></i></a>
+                    <a href="{{Storage::url($peserta->peserta_profil->url_sk_kemenkumham)}}" target="_blank">
+                      <i class="fa fa-download" aria-hidden="true" style="color: #552525; border: 2px solid #552525; border-radius: 8px; padding: 0.3rem"></i>
+                    </a>
                     @endif
                     <div class="input-group custom-file-button">
                       <label class="input-group-text px-4" for="inputGroupFile3">Unggah</label>
@@ -78,7 +84,9 @@
                   </div>
                   <div class="container col-md-8 pe-5">
                     @if ($peserta->peserta_profil->url_kewenangan_kebijakan)
-                    <a href="{{Storage::url($peserta->peserta_profil->url_kewenangan_kebijakan)}}" style="border: 1px solid #552525; color: #552525; padding-block: 0.5rem; font-size: 1.25rem;" class="form-control form-control-lg text-center "><i class="fa fa-download"></i></a>
+                    <a href="{{Storage::url($peserta->peserta_profil->url_kewenangan_kebijakan)}}" target="_blank">
+                      <i class="fa fa-download" aria-hidden="true" style="color: #552525; border: 2px solid #552525; border-radius: 8px; padding: 0.3rem"></i>
+                    </a>
                     @endif
                     <div class="input-group custom-file-button">
                       <label class="input-group-text px-4" for="inputGroupFile4">Unggah</label>
