040af9466b1cc4b5c41473b17b60b675f900a4a2
christ-saragih <bennefit.19@gmail.com>
2024-05-20 21:03:46 +0700

    CREATED: Tampilan panduan bagian Peserta

--- resources/views/peserta/panduan/index.blade.php
+++ resources/views/peserta/panduan/index.blade.php
@@ -11,4 +11,7 @@
       color: #999999 !important;
     }
 </style>
+
+<h3 class="my-4">Berikut panduan untuk mendaftar SNI Award 2024😉</h3>
+<embed src="{{ asset('assets') }}/peserta/pdf-panduan-pendaftaran.pdf" type="application/pdf" width="100%" height="600px" />
 @endsection('content')
