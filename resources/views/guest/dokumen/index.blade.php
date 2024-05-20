50c60ba289bf00fac079f8fbd3b2d1e2af555972
shintaarafah <shintaarafah@gmail.com>
2024-05-20 20:01:53 +0700

    FIXING: Tampilan dokumen frontpage

--- resources/views/guest/dokumen/index.blade.php
+++ resources/views/guest/dokumen/index.blade.php
@@ -6,19 +6,35 @@
         <h1 class="text-center">Dokumen Kegiatan</h1>
         <hr style="width: 350px;">
     </div>
-    <div class="row">
-        <div class="owl-carousel owl-theme">
-            @foreach ($penjadwalan_dokumen as $item)
-            <div class="item">
-                {{-- <a href="{{ route('penjadwalan_dokumen.download', $item->id) }}" class="card-body" style="text-decoration: none" download> --}}
-                <a href="{{ $item->file_dokumen }}" target="_blank" class="card-body" style="text-decoration: none">
-                    <img src="http://127.0.0.1:8000/assets/images/icon/dokumen-kegiatan/dok2.svg" alt="">
-                    <h4>{{ $item->nama_dokumen }}</h4>
-                </a>
-            </div>
-            @endforeach
 
-        </div>
+    <div class="row">
+        @if ($penjadwalan_dokumen->count() <= 2)
+            {{-- Jika data kurang dari atau sama dengan 2, gunakan div --}}
+            <div class="d-flex justify-content-center">
+            @foreach ($penjadwalan_dokumen as $item)
+
+                <div class="item" style="width: 405px;">
+                    <a href="{{ $item->file_dokumen }}" target="_blank" class="card-body" style="text-decoration: none">
+                        <img src="http://127.0.0.1:8000/assets/images/icon/dokumen-kegiatan/dok2.svg" alt="">
+                        <h4>{{ $item->nama_dokumen }}</h4>
+                    </a>
+                </div>
+
+            @endforeach
+            </div>
+        @else
+            {{-- Jika data lebih dari 2, gunakan carousel --}}
+            <div class="owl-carousel owl-theme">
+                @foreach ($penjadwalan_dokumen as $item)
+                    <div class="item">
+                        <a href="{{ $item->file_dokumen }}" target="_blank" class="card-body" style="text-decoration: none">
+                            <img src="http://127.0.0.1:8000/assets/images/icon/dokumen-kegiatan/dok2.svg" alt="">
+                            <h4>{{ $item->nama_dokumen }}</h4>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @endif
     </div>
 </article>
 
