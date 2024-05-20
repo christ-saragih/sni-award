98c96433b318b717a1991159cea1ad7395c22397
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-05-20 11:13:15 +0700

    UPDATE : update data penjadwalan dokumen

--- app/Http/Controllers/PenjadwalanDokumenController.php
+++ app/Http/Controllers/PenjadwalanDokumenController.php
@@ -31,12 +31,12 @@ class PenjadwalanDokumenController extends Controller
     {
         $request->validate([
             'nama_dokumen' => 'required',
-            'file_dokumen' => 'required|file|mimes:pdf|max:2048',
+            'file_dokumen' => 'required|file|mimes:pdf|max:5120',
         ], [
             'nama_dokumen.required' => 'Nama Dokumen Wajib Diisi!',
             'file_dokumen.file' => 'File yang di upload harus berupa file!',
             'file_dokumen.mimes' => 'File harus berupa bertipe: .pdf!',
-            'file_dokumen.max' => 'File tidak boleh lebih dari 2MB!',
+            'file_dokumen.max' => 'File tidak boleh lebih dari 5MB!',
             'file_dokumen.required' => 'File Dokumen Wajib Diisi!'
         ]);
 
@@ -75,19 +75,33 @@ class PenjadwalanDokumenController extends Controller
     public function update(Request $request, PenjadwalanDokumen $penjadwalan_dokumen)
     {
         $request->validate([
-            'nama_dokumen' => 'required',
-            'file_dokumen' => 'required|file|mimes:pdf|max:2048',
+            'file_dokumen' => 'file|mimes:pdf|max:5120',
         ], [
-            'nama_dokumen.required' => 'Nama Dokumen Wajib Diisi!',
             'file_dokumen.file' => 'File yang di upload harus berupa file!',
             'file_dokumen.mimes' => 'File harus berupa bertipe: .pdf!',
-            'file_dokumen.max' => 'File tidak boleh lebih dari 2MB!',
-            'file_dokumen.required' => 'File Dokumen Wajib Diisi!'
+            'file_dokumen.max' => 'File tidak boleh lebih dari 5MB!',
         ]);
 
+        $nama_file = $penjadwalan_dokumen->file_dokumen;
+        if ($request->hasFile('file_dokumen')) {
+            // Delete old image if exists
+            if ($penjadwalan_dokumen->file_dokumen) {
+                $oldImagePath = storage_path('app/public/images/frontpage/dokumen_penjadwalan/' . $penjadwalan_dokumen->file_dokumen);
+                if (file_exists($oldImagePath)) {
+                    unlink($oldImagePath);
+                }
+            }
+
+            // Upload new image
+            $file_dokumen = $request->file('file_dokumen');
+            $dokumen_ekstensi = $file_dokumen->extension();
+            $nama_file = date('ymdhis') . '.' . $dokumen_ekstensi;
+            $file_dokumen->move(storage_path('app/public/images/frontpage/dokumen_penjadwalan/'), $nama_file);
+        }
+
         $penjadwalan_dokumen->update([
-            'nama' => $request->nama,
-            'status' => $request->status,
+            'nama_dokumen' => $request->nama_dokumen,
+            'file_dokumen' => $nama_file,
         ]);
 
         return redirect()->back()->with('success', 'Dokumen Berhasi Diubah');
