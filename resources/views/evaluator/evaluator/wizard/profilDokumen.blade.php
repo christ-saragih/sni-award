57b2ac314e356245bf1d78b27988aa7d6179b8f9
lafathalfath <iqnaraidan12@gmail.com>
2024-05-25 18:43:38 +0700

    UPDATE: copy view and function each role each page

--- resources/views/evaluator/evaluator/wizard/profilDokumen.blade.php
+++ resources/views/evaluator/evaluator/wizard/profilDokumen.blade.php
@@ -241,16 +241,6 @@
                     </div>
                 @endif
             </div>
-            <form action="{{ route('evaluator.evaluator.detail.dokumen.send_feedback', request()->registrasi_id) }}" method="POST" class="chat-input mt-4">
-                @csrf
-                @method('PUT')
-                <textarea name="feedback" id="documentFeedback" placeholder="Tuliskan Pesan" rows="1" oninput="autoResizeTextarea(this)"></textarea>
-                <button type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-send-fill p-0 m-0" viewBox="0 0 18 16">
-                        <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
-                    </svg>
-                </button>
-            </form>
         </div>
     </div>
     {{-- end chatbox --}}
