d9e5882de799c53817f4b4e22f22890fabf079bc
christ-saragih <bennefit.19@gmail.com>
2024-05-27 22:28:16 +0700

    FIXING: Sidebar admin

--- public/assets/admin/js/script.js
+++ public/assets/admin/js/script.js
@@ -140,12 +140,20 @@ navLinkDataMaster.addEventListener("click", function (event) {
     event.preventDefault();
 });
 
-// Href untuk informasi
-// berita
-const navLinkBerita = document.getElementById("navLinkBerita");
-navLinkBerita.style.cursor = "pointer";
-navLinkBerita.addEventListener("click", function (event) {
-    window.location.href = "/admin/berita";
+// Href untuk halaman depan
+// beranda
+const navLinkBeranda = document.getElementById("navLinkBeranda");
+navLinkBeranda.style.cursor = "pointer";
+navLinkBeranda.addEventListener("click", function (event) {
+    window.location.href = "/admin/frontpage";
+    event.preventDefault();
+});
+
+// faq
+const navLinkFaq = document.getElementById("navLinkFaq");
+navLinkFaq.style.cursor = "pointer";
+navLinkFaq.addEventListener("click", function (event) {
+    window.location.href = "/admin/faq";
     event.preventDefault();
 });
 
@@ -154,6 +162,38 @@ const navLinkAcara = document.getElementById("navLinkAcara");
 navLinkAcara.style.cursor = "pointer";
 navLinkAcara.addEventListener("click", function (event) {
     window.location.href = "/admin/acara";
+    event.preventDefault();
+});
+
+// berita
+const navLinkBerita = document.getElementById("navLinkBerita");
+navLinkBerita.style.cursor = "pointer";
+navLinkBerita.addEventListener("click", function (event) {
+    window.location.href = "/admin/berita";
+    event.preventDefault();
+});
+
+// dokumentasi
+const navLinkDokumentasi = document.getElementById("navLinkDokumentasi");
+navLinkDokumentasi.style.cursor = "pointer";
+navLinkDokumentasi.addEventListener("click", function (event) {
+    window.location.href = "/admin/dokumentasi";
+    event.preventDefault();
+});
+
+// linimasa
+const navLinkLinimasa = document.getElementById("navLinkLinimasa");
+navLinkLinimasa.style.cursor = "pointer";
+navLinkLinimasa.addEventListener("click", function (event) {
+    window.location.href = "/admin/linimasa";
+    event.preventDefault();
+});
+
+// kontak
+const navLinkKontak = document.getElementById("navLinkKontak");
+navLinkKontak.style.cursor = "pointer";
+navLinkKontak.addEventListener("click", function (event) {
+    window.location.href = "/admin/kontak";
     event.preventDefault();
 });
 
