60b6e923fb3a3ce7f49632d44b6374e270f09c13
christ-saragih <bennefit.19@gmail.com>
2024-06-18 20:30:56 +0700

    CREATED: Responsive design document and news guest

--- resources/views/guest/berita/index.blade.php
+++ resources/views/guest/berita/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <article id="news">
-    <div class="artikel-container row">
+    <div class="artikel-container">
         <div class="col-12 d-flex align-items-center justify-content-start">
             <div class="title-container d-flex flex-column align-items-center justify-content-center">
                 <h1 style="text-align: left;">Berita</h1>
@@ -10,55 +10,57 @@
             </div>
         </div>
         @if ($berita_terbaru)
-        <div class="content-container col-6">
-            <div class="content row">
-                <h2>Berita Terkini</h2>
-                <img src="{{ asset('storage/images/gambar_berita/' . $berita_terbaru->file_gambar) }}" class="p-0" alt="">
-                {{-- <img src="http://127.0.0.1:8000/assets/images/dokumentasi/berita-terkini.png" class="p-0" alt=""> --}}
-            </div>
-        </div>
-        <div class="content-container col-6">
-            <div class="content row">
-                <h3>{{ $berita_terbaru->judul_berita }}</h3>
-                {{-- <h3>SEMINAR NASIONAL PERINGATAN HARI STANDAR DUNIA</h3> --}}
-                <div class="author-container d-flex">
-                    <div class="flex-grow-1"></div>
-                    <div class="author-content justify-content-end align-items-center">
-                        <p class="hour">1 Hour Ago</p>
-                        <p class="author">
-                            Ditinjau oleh <a href="">Shinta Arafah</a>
-                        </p>
-                    </div>
+        <div class="berita-terkini-container d-flex justify-content-between">
+            <div class="content-container">
+                <div class="content row">
+                    <h2>Berita Terkini</h2>
+                    <img src="{{ asset('storage/images/gambar_berita/' . $berita_terbaru->file_gambar) }}" class="p-0" alt="">
+                    {{-- <img src="http://127.0.0.1:8000/assets/images/dokumentasi/berita-terkini.png" class="p-0" alt=""> --}}
                 </div>
-
-                <p class="description">
-                    {{-- {{ $berita_terbaru_deskripsi }} --}}
-                    {!! $berita_terbaru_deskripsi !!}
-                    {{-- {{ Illuminate\Support\Str::limit($berita_terbaru->deskripsi, 100) }} --}}
-                    <a href="{{ route('informasi.berita.detail', $berita_terbaru->slug) }}">baca selengkapnya.</a>
-                </p>
-                    {{-- BSN akan mengadakan Seminar Nasional dalam rangka Peringatan
-                    Hari Standar Dunia dan Bulan Mutu Nasional (BMN) 2021. Seminar
-                    akan dilaksanakan pada bulan November 2021 di Bandung. Acara
-                    akan dilaksanakan baik secara tatap muka dengan menerapkan
-                    protokol kesehatan maupun daring. PIC Kegiatan: Direktorat
-                    Penguatan Standar dan Penilaian Kesesuaian
-                    <a href="">baca selengkapnya.</a>
-                </p> --}}
-                <div class="program-bsn-container row justify-content-end">
-                    @foreach ($tag_berita_terbaru as $tbt)
-                    <div class="program-bsn col-1 rounded-1 text-center d-flex justify-content-center align-items-center ms-2">
-                        <p>#{{$tbt->tag_berita->nama}}</p>
+            </div>
+            <div class="content-container">
+                <div class="content row">
+                    <h3>{{ $berita_terbaru->judul_berita }}</h3>
+                    {{-- <h3>SEMINAR NASIONAL PERINGATAN HARI STANDAR DUNIA</h3> --}}
+                    <div class="author-container d-flex">
+                        <div class="flex-grow-1"></div>
+                        <div class="author-content justify-content-end align-items-center">
+                            <p class="hour">1 Hour Ago</p>
+                            <p class="author">
+                                Ditinjau oleh <a href="">Shinta Arafah</a>
+                            </p>
+                        </div>
                     </div>
-                    @endforeach
-                    {{-- <div class="program-bsn col-1 rounded-1 text-center d-flex justify-content-center align-items-center ms-2">
-                        <p>SNIBSN</p>
-                    </div>
-                    <div class="program-bsn col-2 rounded-1 text-center d-flex justify-content-center align-items-center ms-2">
-                        <p>SNIAWards2023</p>
-                    </div>
-                    <div class="program-bsn col-2 rounded-1 text-center d-flex justify-content-center align-items-center ms-2">
-                        <p>#BerkasSNI</p> --}}
+    
+                    <p class="description">
+                        {{-- {{ $berita_terbaru_deskripsi }} --}}
+                        {!! $berita_terbaru_deskripsi !!}
+                        {{-- {{ Illuminate\Support\Str::limit($berita_terbaru->deskripsi, 100) }} --}}
+                        <a href="{{ route('informasi.berita.detail', $berita_terbaru->slug) }}">baca selengkapnya.</a>
+                    </p>
+                        {{-- BSN akan mengadakan Seminar Nasional dalam rangka Peringatan
+                        Hari Standar Dunia dan Bulan Mutu Nasional (BMN) 2021. Seminar
+                        akan dilaksanakan pada bulan November 2021 di Bandung. Acara
+                        akan dilaksanakan baik secara tatap muka dengan menerapkan
+                        protokol kesehatan maupun daring. PIC Kegiatan: Direktorat
+                        Penguatan Standar dan Penilaian Kesesuaian
+                        <a href="">baca selengkapnya.</a>
+                    </p> --}}
+                    <div class="program-bsn-container row justify-content-end">
+                        @foreach ($tag_berita_terbaru as $tbt)
+                        <div class="program-bsn col-1 rounded-1 text-center d-flex justify-content-center align-items-center ms-2">
+                            <p>#{{$tbt->tag_berita->nama}}</p>
+                        </div>
+                        @endforeach
+                        {{-- <div class="program-bsn col-1 rounded-1 text-center d-flex justify-content-center align-items-center ms-2">
+                            <p>SNIBSN</p>
+                        </div>
+                        <div class="program-bsn col-2 rounded-1 text-center d-flex justify-content-center align-items-center ms-2">
+                            <p>SNIAWards2023</p>
+                        </div>
+                        <div class="program-bsn col-2 rounded-1 text-center d-flex justify-content-center align-items-center ms-2">
+                            <p>#BerkasSNI</p> --}}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -70,11 +72,9 @@
         @endif
     </div>
 
-</article>
-<div class="container">
-    <div class="row justify-content-center gap-4">
+    <div class="semua-berita-container d-flex justify-content-between flex-wrap">
         @foreach ($berita as $ber)
-        <div class="berita-container col-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
+        <div class="berita-container rounded-3 p-0">
             <img src="{{ asset('storage/images/gambar_berita/' . $ber->file_gambar) }}" class="w-100 rounded-3" alt="">
             <div class="content">
                 <h4>
@@ -88,7 +88,7 @@
             </div>
         </div>
         @endforeach
-        {{-- <div class="berita-container col-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
+        {{-- <div class="berita-containecol-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
             <img src="http://127.0.0.1:8000/assets/images/dokumentasi/berita.png" class="w-100 rounded-3" alt="">
             <div class="content">
                 <h4>
@@ -114,7 +114,7 @@
                 <button class="btn">Baca Selengkapnya</button>
             </div>
         </div>
-        <div class="berita-container col-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
+        <div class="berita-containecol-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
             <img src="http://127.0.0.1:8000/assets/images/dokumentasi/berita.png" class="w-100 rounded-3" alt="">
             <div class="content">
                 <h4>
@@ -132,7 +132,7 @@
                 <button class="btn">Baca Selengkapnya</button>
             </div>
         </div>
-        <div class="berita-container col-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
+        <div class="berita-containecol-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
             <img src="http://127.0.0.1:8000/assets/images/dokumentasi/berita.png" class="w-100 rounded-3" alt="">
             <div class="content">
                 <h4>
@@ -150,7 +150,7 @@
                 <button class="btn">Baca Selengkapnya</button>
             </div>
         </div>
-        <div class="berita-container col-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
+        <div class="berita-containecol-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
             <img src="http://127.0.0.1:8000/assets/images/dokumentasi/berita.png" class="w-100 rounded-3" alt="">
             <div class="content">
                 <h4>
@@ -168,7 +168,7 @@
                 <button class="btn">Baca Selengkapnya</button>
             </div>
         </div>
-        <div class="berita-container col-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
+        <div class="berita-containecol-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
             <img src="http://127.0.0.1:8000/assets/images/dokumentasi/berita.png" class="w-100 rounded-3" alt="">
             <div class="content">
                 <h4>
@@ -186,7 +186,7 @@
                 <button class="btn">Baca Selengkapnya</button>
             </div>
         </div>
-        <div class="berita-container col-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
+        <div class="berita-containecol-lg-4 col-md-6 col-sm-12 rounded-3 p-0">
             <img src="http://127.0.0.1:8000/assets/images/dokumentasi/berita.png" class="w-100 rounded-3" alt="">
             <div class="content">
                 <h4>
@@ -205,5 +205,7 @@
             </div>
         </div> --}}
     </div>
-</div>
+
+</article>
+
 @endsection
