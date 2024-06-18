21351b158d3984085e2a39eb55375855338e6bdb
christ-saragih <bennefit.19@gmail.com>
2024-06-19 02:37:48 +0700

    CREATED: Responsive design detail berita guest

--- resources/views/guest/berita/detail.blade.php
+++ resources/views/guest/berita/detail.blade.php
@@ -1,132 +1,217 @@
 @extends('guest.layouts.master')
 
 @section('content')
+<style>
+    .container-viewer {
+        display: block;
+        float: right;
+    }
+    #informasi .content {
+        padding-left: 0px !important;
+        padding-inline: 90px !important;
+    }
+    #informasi .content .container-title-berita {
+        width: 60%;
+        padding-right: 90px;
+    }
+    #informasi .content .accordion {
+        width: 40%;
+    }
+    .container-berita .date {
+        float: right;
+    }
+    .container-berita .berita {
+            gap: 20px;
+            margin-bottom: 10px;
+        }
+    .container-berita .berita .container-image {
+            width: 30%;
+        }
+    .container-berita .berita .container-title {
+            width: 70%;
+        }
+
+    .content-bawah .content {
+        width: 60%;
+    }
+    .content-bawah .content-samping {
+        width: 40%;
+        margin-top: -85px;
+    }
+    .container-berita .container-tanggal {
+            display: none;
+        }
+        .container-berita .container-title .date {
+            display: flex;
+        }
+    
+    @media (max-width: 480px) {
+        .container-viewer {
+            display: flex;
+            align-items: center;
+            gap: 0.75rem;
+            float: left !important;
+        }
+        #informasi {
+            margin-top: -60px;
+        }
+        #informasi > .content {
+            padding-top: 100px;
+            gap: 10px;
+        }
+        #informasi .content .container-title-berita {
+            width: 70% !important;
+            padding-right: 0px;
+        }
+        #informasi .content .accordion {
+            width: 30% !important;
+        }
+        #informasi .content {
+            padding-inline: 10px !important;
+        }
+        #informasi .content-samping {
+            padding-left: 10px !important;
+            padding-right: 10px !important;
+        }
+        .content-samping .container-berita {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 10px;
+            padding-inline: 0px !important;
+        }
+        .container-berita .date {
+            float: left !important;
+        }
+        
+        .container-berita .berita {
+            flex-direction: column !important; 
+        }
+        .container-title {
+            flex-grow: 1 !important;
+            width: 100% !important;
+            padding-inline: 2px !important;
+        }
+        .container-tanggal {
+            padding-inline: 2px !important;
+        }
+        .content-bawah {
+            flex-wrap: wrap;
+        }
+        .content-bawah .content {
+            width: 100% !important;
+        }
+        .content-bawah .content-samping {
+            width: 100% !important;
+            margin-top: 10px !important;
+        }
+        .accordion-button {
+            font-size: 14px !important;
+            padding: 12px 10px !important;
+        }
+        .container-berita .berita .container-image {
+            width: 100% !important;
+            height: 110px;
+        }
+        .container-berita .berita .container-image img {
+            width: 100% !important;
+            height: 100%;
+        }
+        .container-berita .container-tanggal {
+            display: flex !important;
+            
+        }
+        .container-berita .container-title .date {
+            display: none !important;
+        }
+        .container-berita .container-tanggal p {
+            margin: 0px;
+        }
+        p.title {
+            margin: 0;
+        }
+        .container-berita .berita {
+            flex-direction: row;
+            /* justify-content: space-between !important; */
+            /* align-content: space-between !important; */
+            gap: 5px !important;
+        }
+    }
+</style>
+
 <article id="informasi" class="shape-bawah">
-    <div class="content row" style="max-width: 100%">
-        <h3 class="col-7">{{ $berita->judul_berita }}</h3>
-        {{-- <h3 class="col-7">Seminar Nasional Peringatan Hari Standar Dunia</h3> --}}
-    </div>
-    <div class="row m-0 d-flex" style="max-width: 100%">
-        <div class="content col-lg-7">
-            {{-- <hr> --}}
-            <div class="row float-end mb-4">
-                <div class="d-flex mb-2">
+    <div class="d-flex content" style="max-width: 100%; margin-bottom: 20px;">
+        <div class="container-title-berita">
+            <h3 class="">{{ $berita->judul_berita }}</h3>
+            <hr class="w-100"/>
+            <div class="container-viewer">
+                <div class="d-flex align-items-center">
                     <img src="{{ asset('assets') }}/images/icon/calendar.svg" class="me-2" alt="">
                     <p class="mb-0">{{ $tanggal }}</p>
                     {{-- <p class="mb-0">{{ \Carbon\Carbon::parse($berita->tanggal)->locale('id')->translatedFormat('l, j F Y') }}</p> --}}
                     {{-- <p class="mb-0">Senin, 12 Februari 2024</p> --}}
                 </div>
-                <div class="d-flex">
+                <div class="d-flex align-items-center">
                     <img src="{{ asset('assets') }}/images/icon/eye.svg" class="me-2" alt="">
                     <p class="mb-0">470</p>
                 </div>
             </div>
-            {{-- <img src="{{ asset('storage/images/gambar_berita/' . $berita->file_gambar) }}" alt="" class="berita mb-4" style="object-fit: cover;"> --}}
-            {{-- <img src="{{ asset('assets') }}/images/dokumentasi/berita-terkini.png" class="berita mb-4" alt=""> --}}
-            <p class="description">{!! $berita->deskripsi !!}</p>
-            {{-- <p class="description">BSN akan mengadakan Seminar Nasional dalam rangka Peringatan Hari Standar Dunia dan Bulan Mutu Nasional (BMN) 2021. Seminar akan dilaksanakan pada bulan November 2021 di Bandung. Acara akan dilaksanakan baik secara tatap muka dengan menerapkan protokol kesehatan maupun daring. PIC Kegiatan: Direktorat Penguatan Standar dan Penilaian Kesesuaian . BSN akan mengadakan Seminar Nasional dalam rangka Peringatan Hari Standar Dunia dan Bulan Mutu Nasional (BMN) 2021. Seminar akan dilaksanakan pada bulan November 2021 di Bandung. Acara akan dilaksanakan baik secara tatap muka dengan menerapkan protokol kesehatan maupun daring. PIC Kegiatan: Direktorat Penguatan Standar dan Penilaian Kesesuaian.</p> --}}
         </div>
-        <div class="content-samping col-lg-5">
-            <div class="row ms-3">
-                {{-- <div class="accordion" id="accordionExample">
-                    <div class="accordion-item float-end">
-                    <h2 class="accordion-header" id="headingThree">
-                        <button
-                        class="accordion-button collapsed me-4"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#collapseThree"
-                        aria-expanded="false"
-                        aria-controls="collapseThree"
-                        >
-                    Tag Populer
-                        </button>
-                    </h2>
-                    <div
-                        id="collapseThree"
-                        class="accordion-collapse collapse"
-                        aria-labelledby="headingThree"
-                        data-bs-parent="#accordionExample"
-                    >
-                        <div class="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is
-                        hidden by default, until the collapse plugin adds the appropriate
-                        classes that we use to style each element. These classes control
-                        the overall appearance, as well as the showing and hiding via CSS
-                        transitions. You can modify any of this with custom CSS or
-                        overriding our default variables. It's also worth noting that just
-                        about any HTML can go within the <code>.accordion-body</code>,
-                        though the transition does limit overflow.
+
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item float-end">
+            <h2 class="accordion-header" id="headingThree">
+                <button
+                class="accordion-button collapsed me-4"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+                >
+            Tag Populer
+                </button>
+            </h2>
+            <div
+                id="collapseThree"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingThree"
+                data-bs-parent="#accordionExample"
+            >
+                <div class="accordion-body">
+                <strong>This is the third</strong>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="m-0 d-flex content-bawah" style="max-width: 100%">
+        <div class="content">
+            <img src="{{ asset('storage/images/gambar_berita/' . $berita->file_gambar) }}" alt="" class="berita mb-4" style="object-fit: cover;">
+            <p class="description">{!! $berita->deskripsi !!}</p>
+        </div>
+        <div class="content-samping">
+            
+                <div class="d-flex flex-column justify-content-start align-items-start">
+                    <h3 class="ms-2">Berita</h3>
+                    <hr style="margin-inline: 0px; width: 32%; margin-bottom: 25px;">
+                </div>
+                <div class="container-berita">
+                    @foreach ($get_all_berita as $ber)
+                    <div class="d-flex berita">
+                        <div class="container-image">
+                            <img src="{{ asset('storage/images/gambar_berita/' . $ber->file_gambar) }}" alt="" style="object-fit: cover;">
+                        </div>
+                        <div class="container-title d-flex flex-column justify-content-between">
+                            <p class="title">{{ $ber->judul_berita }}</p>
+                            <p class="date items-end me-4" style="align-self: flex-end">{{ $ber->tanggal }}</p>
+                        </div>
+                        <div class="container-tanggal">
+                            <p class="date float-end me-4">{{ $ber->tanggal }}</p>
                         </div>
                     </div>
-                    </div>
-                </div> --}}
-
-                {{-- <h3 class="ms-2">Berita</h3> --}}
-                {{-- <hr> --}}
-
-                @foreach ($get_all_berita as $ber)
-                <div class="row my-4">
-                    <div class="col-12 col-lg-4">
-                        <img src="{{ asset('storage/images/gambar_berita/' . $ber->file_gambar) }}" alt="" style="object-fit: cover;">
-                    </div>
-                    <div class="col-12 col-lg-8">
-                        <p class="title">{{ $ber->judul_berita }}</p>
-                        <p class="date float-end me-4">{{ $ber->tanggal }}</p>
-                    </div>
+                    @endforeach
                 </div>
-                @endforeach
-
-                {{-- <div class="row my-4">
-                    <div class="col-4">
-                        <img src="{{ asset('assets') }}/images/berita/berita1.png" alt="">
-                    </div>
-                    <div class="col-8">
-                        <p class="title">Perusahaan/Organisasi Terbaik Penerap SNI, Raih SNI Award 2021</p>
-                        <p class="date float-end me-4">Senin, 14 Februari 2024</p>
-                    </div>
-                </div>
-
-                <div class="row my-4">
-                    <div class="col-4">
-                        <img src="{{ asset('assets') }}/images/berita/berita1.png" alt="">
-                    </div>
-                    <div class="col-8">
-                        <p class="title">Perusahaan/Organisasi Terbaik Penerap SNI, Raih SNI Award 2021</p>
-                        <p class="date float-end me-4">Senin, 14 Februari 2024</p>
-                    </div>
-                </div>
-
-                <div class="row my-4">
-                    <div class="col-4">
-                        <img src="{{ asset('assets') }}/images/berita/berita1.png" alt="">
-                    </div>
-                    <div class="col-8">
-                        <p class="title">Perusahaan/Organisasi Terbaik Penerap SNI, Raih SNI Award 2021</p>
-                        <p class="date float-end me-4">Senin, 14 Februari 2024</p>
-                    </div>
-                </div>
-
-                <div class="row my-4">
-                    <div class="col-4">
-                        <img src="{{ asset('assets') }}/images/berita/berita1.png" alt="">
-                    </div>
-                    <div class="col-8">
-                        <p class="title">Perusahaan/Organisasi Terbaik Penerap SNI, Raih SNI Award 2021</p>
-                        <p class="date float-end me-4">Senin, 14 Februari 2024</p>
-                    </div>
-                </div>
-
-                <div class="row my-4">
-                    <div class="col-4">
-                        <img src="{{ asset('assets') }}/images/berita/berita1.png" alt="">
-                    </div>
-                    <div class="col-8">
-                        <p class="title">Perusahaan/Organisasi Terbaik Penerap SNI, Raih SNI Award 2021</p>
-                        <p class="date float-end me-4">Senin, 14 Februari 2024</p>
-                    </div>
-                </div> --}}
-
 
                 <div class="col mt-5">
                 <a href="/informasi/berita" class="btn">Baca Lainnya</a>
@@ -136,7 +221,6 @@
             </div>
 
 
-        </div>
     </div>
 </article>
 @endsection('content')
