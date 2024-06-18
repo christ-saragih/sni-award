800e125fb05a66facfa18d32141e9f77d21f4796
christ-saragih <bennefit.19@gmail.com>
2024-06-18 21:56:51 +0700

    FIXING: Responsive design frontpage (Berita)

--- resources/views/guest/berita/detail.blade.php
+++ resources/views/guest/berita/detail.blade.php
@@ -7,8 +7,8 @@
         {{-- <h3 class="col-7">Seminar Nasional Peringatan Hari Standar Dunia</h3> --}}
     </div>
     <div class="row m-0 d-flex" style="max-width: 100%">
-        <div class="content col-7">
-            <hr>
+        <div class="content col-lg-7">
+            {{-- <hr> --}}
             <div class="row float-end mb-4">
                 <div class="d-flex mb-2">
                     <img src="{{ asset('assets') }}/images/icon/calendar.svg" class="me-2" alt="">
@@ -21,14 +21,14 @@
                     <p class="mb-0">470</p>
                 </div>
             </div>
-            <img src="{{ asset('storage/images/gambar_berita/' . $berita->file_gambar) }}" class="berita mb-4" alt="">
+            {{-- <img src="{{ asset('storage/images/gambar_berita/' . $berita->file_gambar) }}" alt="" class="berita mb-4" style="object-fit: cover;"> --}}
             {{-- <img src="{{ asset('assets') }}/images/dokumentasi/berita-terkini.png" class="berita mb-4" alt=""> --}}
             <p class="description">{!! $berita->deskripsi !!}</p>
             {{-- <p class="description">BSN akan mengadakan Seminar Nasional dalam rangka Peringatan Hari Standar Dunia dan Bulan Mutu Nasional (BMN) 2021. Seminar akan dilaksanakan pada bulan November 2021 di Bandung. Acara akan dilaksanakan baik secara tatap muka dengan menerapkan protokol kesehatan maupun daring. PIC Kegiatan: Direktorat Penguatan Standar dan Penilaian Kesesuaian . BSN akan mengadakan Seminar Nasional dalam rangka Peringatan Hari Standar Dunia dan Bulan Mutu Nasional (BMN) 2021. Seminar akan dilaksanakan pada bulan November 2021 di Bandung. Acara akan dilaksanakan baik secara tatap muka dengan menerapkan protokol kesehatan maupun daring. PIC Kegiatan: Direktorat Penguatan Standar dan Penilaian Kesesuaian.</p> --}}
         </div>
-        <div class="content-samping col-5">
+        <div class="content-samping col-lg-5">
             <div class="row ms-3">
-                <div class="accordion" id="accordionExample">
+                {{-- <div class="accordion" id="accordionExample">
                     <div class="accordion-item float-end">
                     <h2 class="accordion-header" id="headingThree">
                         <button
@@ -60,17 +60,17 @@
                         </div>
                     </div>
                     </div>
-                </div>
+                </div> --}}
 
-                <h3 class="ms-2">Berita</h3>
-                <hr>
+                {{-- <h3 class="ms-2">Berita</h3> --}}
+                {{-- <hr> --}}
 
                 @foreach ($get_all_berita as $ber)
                 <div class="row my-4">
-                    <div class="col-4">
-                        <img src="{{ asset('storage/images/gambar_berita/' . $ber->file_gambar) }}" alt="">
+                    <div class="col-12 col-lg-4">
+                        <img src="{{ asset('storage/images/gambar_berita/' . $ber->file_gambar) }}" alt="" style="object-fit: cover;">
                     </div>
-                    <div class="col-8">
+                    <div class="col-12 col-lg-8">
                         <p class="title">{{ $ber->judul_berita }}</p>
                         <p class="date float-end me-4">{{ $ber->tanggal }}</p>
                     </div>
